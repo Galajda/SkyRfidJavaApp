3d4ffb06a182136c9d13615640c00eebce50128a
Galajda <33378775+Galajda@users.noreply.github.com>
2017-12-30 14:56:48 -0600

    Added ok/cancel dialog box to the class. This method is an instance method. A static method is preferable. I do not know if it is possible.

--- src/skyrfidjavaapp/FxMsgBox.java
+++ src/skyrfidjavaapp/FxMsgBox.java
@@ -22,14 +22,24 @@ import javafx.scene.image.Image;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+//import javafx.event.Event;
+//import javafx.event.EventHandler;
 
 /**
  *
  * @author MichalG
  */
 public class FxMsgBox {
+    private static final String BTN_OK_TXT = "OK";
+    private static final String BTN_CANCEL_TXT = "Cancel";
+    private Boolean confirmDialogResult;
+    
+    public FxMsgBox() { confirmDialogResult = false; }
+    
     /**
      * Displays a message box using FX tools
      * @param message The body of the message
@@ -46,7 +56,7 @@ public class FxMsgBox {
         stage.getIcons().add(new Image("skyrfidjavaapp/images/attention.jpg"));
         
         Label msgArea = new Label(message); //how to wrap message?
-        Button btnOk = new Button("OK");
+        Button btnOk = new Button(FxMsgBox.BTN_OK_TXT);
         btnOk.setOnAction(e->stage.close());
         VBox pane = new VBox(40);
         pane.getChildren().addAll(msgArea, btnOk);
@@ -55,4 +65,72 @@ public class FxMsgBox {
         stage.setScene(scene);
         stage.showAndWait();
     }
+    public Boolean confirm(String message, String title) {
+        Stage stage = new Stage();
+//        Boolean response = false;
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle(title);
+        stage.setMinWidth(300);
+        stage.setMaxWidth(900);
+        //set width according to content. 
+        stage.getIcons().add(new Image("skyrfidjavaapp/images/question.jpg"));
+        
+        Label msgArea = new Label(message); //how to wrap message?
+        Button btnOk = new Button(FxMsgBox.BTN_OK_TXT);
+//        btnOk.setOnAction(e -> getAnswer(e, stage));
+//        MyClickHandler ch = new MyClickHandler();
+        btnOk.setOnAction(e -> getAnswer(e, stage));
+        Button btnCancel = new Button(FxMsgBox.BTN_CANCEL_TXT);
+        btnCancel.setOnAction(e -> getAnswer(e, stage));
+        VBox pane = new VBox(20);
+        HBox btnPane = new HBox(10);
+        btnPane.setAlignment(Pos.CENTER);
+        btnPane.getChildren().addAll(btnOk, btnCancel);
+        pane.getChildren().addAll(msgArea, btnPane);
+        pane.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        System.out.println("about to wait");
+        stage.showAndWait(); //thread waits here until button is clicked
+        System.out.println("done waiting");
+//        stage.close();
+        return confirmDialogResult;
+        
+    }
+    private void getAnswer(ActionEvent e, Stage s) {
+//        System.out.println("ok/cancel clicked " + e.getSource());
+        
+        String name = "";
+        if (e.getSource() instanceof Button) {
+            Button b = (Button)e.getSource();
+            name = b.getText();
+//            System.out.println("button name " + name);
+        }
+        confirmDialogResult = name.equals(FxMsgBox.BTN_OK_TXT);
+        s.close();
+//        return (name.equals(FxMsgBox.BTN_OK_TXT));
+    }
+    
+    //anonymous inner class event handler
+    //new EventHandler<ActionEvent>() {
+//                    @Override
+//                    public void handle(ActionEvent event) {
+//                        response = true; var must be final for reference from here
+//                        
+//                        
+//                    }
+    //if the class implements EventHandler, setOnAction must specify this. However, non-static
+    //reference is not allowed in a static method.
+//    @Override
+//    public void handle(Event event) {
+//        System.out.println("handling the click with fxmsgbox implementation");
+//    }
+    
+    //the basic problem is that a click handler has a void return type. you cannot return a value
+//    private static class MyClickHandler implements EventHandler {
+//        @Override
+//        public void handle(Event event) {
+//            System.out.println("handling the click in inner class");
+//        }        
+//    }
 }
